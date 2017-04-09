07a42f5d078868f435b2044cd897aa8afbc812e8
tom <pitel.tom@gmail.com>
2017-04-09 15:05:12 +0200

    MAJ jeu de test pour comparaison avec sortie attendue

--- tests/executeTests.sh
+++ tests/executeTests.sh
@@ -11,20 +11,34 @@ mkdir out
 for i in src/*.c
 
 do
-	echo -e "\nCompilation et execution de "
-	echo $i
+	echo -e "\nCompilation et execution de $i"
 	../Compiler_H4413 < $i
 	gcc main.s
 	./a.out > out/${i:4}.out
 done
+
+let "nbTestFailed = 0"
 
 for i in out/*.out
 
 do
 	echo $i
 	cat $i
+	if [[ $(diff $i expected/${i:4}) ]]; then
+		echo -e "\n==> TEST FAILED"
+		let "nbTestFailed = nbTestFailed+1"
+	else
+		echo -e "\n==> TEST PASSED"
+	fi
 	echo -e "\n"
 done
+
+echo -e "\n\t\t|******************************************|"
+echo -e "\t\t|                                          |"
+echo -e "\t\t|            $nbTestFailed test(s) failed              |"
+echo -e "\t\t|                                          |"
+echo -e "\t\t|******************************************|\n"
+echo -e "\n \n"
 
 rm main.s
 rm a.out
